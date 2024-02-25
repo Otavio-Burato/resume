62b959c0c03353a94653ac11466b42c3808a73fb
Otavio-burato <otavioburato42@gmail.com>
2024-02-25 00:26:00 -0300

    test deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://resume.github.io',
+  base: '/resume',
   integrations: [tailwind()]
 });
